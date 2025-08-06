4b559a341d045d7778a0583ef903c8b1fda27333
CroquetaConQueso <lacio161@gmail.com>
2025-08-06 18:20:06 +0200

    Starting using the annotation of ModelAttributes, looped over a list, gave conditions

---
+++ src/main/java/com/proyectospring/proyecto/models/Usuario.java
@@ -0,0 +1,35 @@
+package com.proyectospring.proyecto.models;
+
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Usuario(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Usuario(){}
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setApellido (String apellido){
+        this.apellido = apellido;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
